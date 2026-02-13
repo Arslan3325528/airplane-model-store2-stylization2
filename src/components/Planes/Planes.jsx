c42818545493c62492ce5c8131a6d6167a0f2bd7
Ruslan379 <3325528@ukr.net>
2026-02-13 14:58:37 +0200

    Next49

--- src/components/Planes/Planes.jsx
+++ src/components/Planes/Planes.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 
 import { FaMapMarkerAlt, FaUserAlt, FaCalendarAlt, FaClock } from 'react-icons/fa'; //! приклад
-import { AiOutlineFlag } from "react-icons/ai";
-// AiOutlineFlag - прапор
+import { AiOutlineFlag, AiOutlineInfoCircle } from "react-icons/ai";
+import {  } from "react-icons/ai";
+// <AiOutlineFlag /> - прапор, країна (country)
+// <AiOutlineInfoCircle /> - опис (description)
 
 import css from "./Planes.module.css";
 
