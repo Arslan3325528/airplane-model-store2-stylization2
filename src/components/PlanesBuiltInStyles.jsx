f4744500d4553ecb5e1389ea99985475a9ea2dba
Ruslan379 <3325528@ukr.net>
2026-02-15 14:15:38 +0200

    Next66

--- src/components/PlanesBuiltInStyles.jsx
+++ src/components/PlanesBuiltInStyles.jsx
@@ -119,7 +119,8 @@ export default function Planes({
 Planes.propTypes = {
   urlMain: PropTypes.string.isRequired,
   urlPromotional: PropTypes.string.isRequired,
-  urlActual: PropTypes.string.isRequired,
+  // urlActual: PropTypes.string.isRequired,
+  urlActual: PropTypes.array,
   nameBrief: PropTypes.string.isRequired,
   nameFull: PropTypes.string.isRequired,
   nickname: PropTypes.string.isRequired,
